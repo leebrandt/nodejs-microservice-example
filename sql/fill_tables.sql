9d8564f45d9bfa752cf469f9a1005eb591c541ca
Lee Brandt <leebrandt@gmail.com>
2022-07-27 12:27:08 -0500

    Tweaks to get the database initializing on docker-compose up

---
+++ sql/fill_tables.sql
@@ -0,0 +1,3 @@
+INSERT INTO products(name, description, price) VALUES('Blistex', 'Lib balm', '2.99');
+INSERT INTO products(name, description, price) VALUES('Chapstick', 'Lib balm', '3.69');
+INSERT INTO products(name, description, price) VALUES('Burts Bees Wax', 'Lib balm', '4.39');
